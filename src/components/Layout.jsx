0f24d244f58310d73ba79cc44778fe1447531dab
JLTC3111 <dr.herbal2011@gmail.com>
2025-04-04 11:46:57 +0700

    update footer

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -35,6 +35,32 @@ export default function Layout(props) {
 
     const footer = (
         <footer>
+            <div className="image-slider">
+            <div className="image-track">
+             <img src="/images/coffee1.png" alt="Coffee 1" onClick={() => handleImageClick('/images/coffee1.png')} />
+             <img src="/images/coffee2.png" alt="Coffee 2" onClick={() => handleImageClick('/images/coffee2.png')} />
+             <img src="/images/coffee3.png" alt="Coffee 3" onClick={() => handleImageClick('/images/coffee3.png')}/>
+             <img src="/images/coffee4.png" alt="Coffee 4" onClick={() => handleImageClick('/images/coffee4.png')}/>
+             <img src="/images/coffee5.png" alt="Coffee 5" onClick={() => handleImageClick('/images/coffee5.png')}/>
+             <img src="/images/coffee6.png" alt="Coffee 6" onClick={() => handleImageClick('/images/coffee6.png')}/>
+             <img src="/images/coffee7.png" alt="Coffee 7" onClick={() => handleImageClick('/images/coffee7.png')}/>
+             <img src="/images/coffee8.png" alt="Coffee 8" onClick={() => handleImageClick('/images/coffee8.png')}/>
+             <img src="/images/coffee9.png" alt="Coffee 9" onClick={() => handleImageClick('/images/coffee9.png')}/>
+             <img src="/images/coffee10.png" alt="Coffee 10" onClick={() => handleImageClick('/images/coffee10.png')}/>
+             <img src="/images/coffee11.png" alt="Coffee 11" onClick={() => handleImageClick('/images/coffee11.png')}/> 
+             <img src="/images/coffee1.png" alt="Coffee 1" onClick={() => handleImageClick('/images/coffee1.png')} />
+             <img src="/images/coffee2.png" alt="Coffee 2" onClick={() => handleImageClick('/images/coffee2.png')} />
+             <img src="/images/coffee3.png" alt="Coffee 3" onClick={() => handleImageClick('/images/coffee3.png')}/>
+             <img src="/images/coffee4.png" alt="Coffee 4" onClick={() => handleImageClick('/images/coffee4.png')}/>
+             <img src="/images/coffee5.png" alt="Coffee 5" onClick={() => handleImageClick('/images/coffee5.png')}/>
+             <img src="/images/coffee6.png" alt="Coffee 6" onClick={() => handleImageClick('/images/coffee6.png')}/>
+             <img src="/images/coffee7.png" alt="Coffee 7" onClick={() => handleImageClick('/images/coffee7.png')}/>
+             <img src="/images/coffee8.png" alt="Coffee 8" onClick={() => handleImageClick('/images/coffee8.png')}/>
+             <img src="/images/coffee9.png" alt="Coffee 9" onClick={() => handleImageClick('/images/coffee9.png')}/>
+             <img src="/images/coffee10.png" alt="Coffee 10" onClick={() => handleImageClick('/images/coffee10.png')}/>
+             <img src="/images/coffee11.png" alt="Coffee 11" onClick={() => handleImageClick('/images/coffee11.png')}/>              
+            </div>
+        </div>
             <p><span className="text-gradient">Caffiend</span> was made by <a target="_blank" href="https://dobaolongicueltd.netlify.app">LD⭐️⭐️⭐️⭐️ </a><br />Check out the project on <a target="_black" href="https://www.github.com/JLTC3111">GitHub</a>!</p>
         </footer>
     )
@@ -53,36 +79,10 @@ export default function Layout(props) {
         setSelectedImage(imageSrc)
         setShowImageModal(true) // Show the modal with the selected image
     }
-    return (
+    return ( 
+       
         <>
-        
-        <div className="image-slider">
-            <div className="image-track">
-             <img src="/images/coffee1.png" alt="Coffee 1" onClick={() => handleImageClick('/images/coffee1.png')} />
-             <img src="/images/coffee2.png" alt="Coffee 2" onClick={() => handleImageClick('/images/coffee2.png')} />
-             <img src="/images/coffee3.png" alt="Coffee 3" onClick={() => handleImageClick('/images/coffee3.png')}/>
-             <img src="/images/coffee4.png" alt="Coffee 4" onClick={() => handleImageClick('/images/coffee4.png')}/>
-             <img src="/images/coffee5.png" alt="Coffee 5" onClick={() => handleImageClick('/images/coffee5.png')}/>
-             <img src="/images/coffee6.png" alt="Coffee 6" onClick={() => handleImageClick('/images/coffee6.png')}/>
-             <img src="/images/coffee7.png" alt="Coffee 7" onClick={() => handleImageClick('/images/coffee7.png')}/>
-             <img src="/images/coffee8.png" alt="Coffee 8" onClick={() => handleImageClick('/images/coffee8.png')}/>
-             <img src="/images/coffee9.png" alt="Coffee 9" onClick={() => handleImageClick('/images/coffee9.png')}/>
-             <img src="/images/coffee10.png" alt="Coffee 10" onClick={() => handleImageClick('/images/coffee10.png')}/>
-             <img src="/images/coffee11.png" alt="Coffee 11" onClick={() => handleImageClick('/images/coffee11.png')}/>  
-             <img src="/images/coffee1.png" alt="Coffee 1" onClick={() => handleImageClick('/images/coffee1.png')}/>
-             <img src="/images/coffee2.png" alt="Coffee 2" onClick={() => handleImageClick('/images/coffee2.png')}/>
-             <img src="/images/coffee3.png" alt="Coffee 3" onClick={() => handleImageClick('/images/coffee3.png')}/>
-             <img src="/images/coffee4.png" alt="Coffee 4" onClick={() => handleImageClick('/images/coffee4.png')}/>
-             <img src="/images/coffee5.png" alt="Coffee 5" onClick={() => handleImageClick('/images/coffee5.png')}/>
-             <img src="/images/coffee6.png" alt="Coffee 6" onClick={() => handleImageClick('/images/coffee6.png')}/>
-             <img src="/images/coffee7.png" alt="Coffee 7" onClick={() => handleImageClick('/images/coffee7.png')}/>
-             <img src="/images/coffee8.png" alt="Coffee 8" onClick={() => handleImageClick('/images/coffee8.png')}/>
-             <img src="/images/coffee9.png" alt="Coffee 9" onClick={() => handleImageClick('/images/coffee9.png')}/>
-             <img src="/images/coffee10.png" alt="Coffee 10" onClick={() => handleImageClick('/images/coffee10.png')}/>
-             <img src="/images/coffee11.png" alt="Coffee 11" onClick={() => handleImageClick('/images/coffee11.png')}/>            
-            </div>
-        </div>
-
+    
             {showAuthModal && (
                 <Modal handleCloseModal={handleCloseModal}>
                     <Authentication handleCloseModal={handleCloseModal} />   
